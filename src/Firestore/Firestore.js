40224786920eac77b5d52b0f74cfe4ab3b5480d7
Your Name <you@example.com>
2023-08-16 23:19:17 +0530

    second commit

--- src/Firestore/Firestore.js
+++ src/Firestore/Firestore.js
@@ -1,14 +1,1 @@
-// import firebase from 'firebase/app'
-// import 'firebase/firestore'
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyA8kA2fWSb2aeEF6s__DRgoHFDaXIldh0k",
-//     authDomain: "learners-app-6399b.firebaseapp.com",
-//     projectId: "learners-app-6399b",
-//     storageBucket: "learners-app-6399b.appspot.com",
-//     messagingSenderId: "1038544310430",
-//     appId: "1:1038544310430:web:503ed9c7b0285a1181ab8f"
-//   };
-
-// firebase.initializeApp(firebaseConfig)
-// export const db = firebase.firestore();
